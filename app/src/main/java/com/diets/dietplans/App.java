a8fd022d7556ce71731b71305c7fe2fbe6677004
Wadim <udovoron@gmail.com>
2021-03-02 22:13:56 +0300

    add analytic

--- app/src/main/java/com/diets/dietplans/App.java
+++ app/src/main/java/com/diets/dietplans/App.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import androidx.multidex.MultiDexApplication;
 
+import com.amplitude.api.Amplitude;
 import com.diets.dietplans.R;
 
 public class App extends MultiDexApplication {
@@ -11,6 +12,8 @@ public class App extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        Amplitude.getInstance().initialize(this, "be460abcf40b08b38ced8fe42d9c9dc6").
+                enableForegroundTracking(this);
 
     }
 }
